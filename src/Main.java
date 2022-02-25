e2c8a6040a4d4dd22813b0350670ac47c7ff99b3
Alexander Zagrebnev <aszagrebnev@gmail.com>
2022-02-25 19:04:58 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        String a = "101";
+        String b = "1010";
+
+        System.out.println(bins.sum(a,b));
+        System.out.println(bins.mult(a,b));
+    }
+}
